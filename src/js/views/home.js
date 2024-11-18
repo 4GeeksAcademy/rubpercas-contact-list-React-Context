d6205dbe9d771383516725ca04bb24ca91300c7d
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-11-18 19:04:10 +0000

    Empty contact list message added

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -12,11 +12,18 @@ export const Home = () => {
             <h1 className="text-center mb-2">YOUR LIST CONTACT</h1>
             <div className="d-flex justify-content-center align-items-center">
                 <ul className="list-group mt-3 w-50">
-                    {store.contactList && store.contactList.length > 0 && store.contactList.map((contact, index) => {
+                    {store.contactList.length > 0 ? (
+                        store.contactList.map((contact, index) => (
+                            <Contact key={index} contact={contact} />
+                        ))
+                    ) : (
+                        <h1 className="text-center mb-2 mt-5">Your contact list is empty. Add some new contacts.</h1>
+                    )}
+                    {/* {store.contactList && store.contactList.length > 0 && store.contactList.map((contact, index) => {
                         return (
                             <Contact contact={contact} key={index} />
                         )
-                    })}
+                    })} */}
                 </ul>
             </div>
         </div>
